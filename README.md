e0f4a4729d19cbb87243374921f7ada7b648c90b
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-07-11 09:58:07 -0400

    Adjusted name of crate; Added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# my_crate_PipoAT
+This is an example crate that is published to crate.io per the official documentation/tutorial provided by Rust
+# functions
+## add_one
+adds one to a given value
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-//! # My Crate
+//! # My Crate PipoAT
 //!
-//! `my_crate` is a collection of utilities to make performing certain
+//! `my_crate_PipoAT` is a collection of utilities to make performing certain
 //! calculations more convenient.
 /// Adds one to the number given.
 ///
